dd592b3f29b2dc39c51f0d960b196c60f05d634f
A.Agadullin <winamp2690@icloud.com>
2020-05-08 23:09:44 +0300

    add

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import "./styles/main.pcss";
+if (process.env.NODE_ENV === "development") {
+  require("file-loader!./index.pug");
+}
+
+import "./scripts/skills";
+import "./scripts/works";
+import "./scripts/parallax";
+import "./scripts/reviews";
